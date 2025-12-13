866ff96d6b3f5e09f15c2adfa0a6108ecb74ca66
MichaelBrown00 <jw95872@gmail.com>
2025-12-13 09:36:45 +0100

    Initial clean PixNest setup

---
+++ script.js
@@ -0,0 +1,6 @@
+const uploadBtn = document.getElementById("uploadBtn");
+const output = document.getElementById("output");
+
+uploadBtn.addEventListener("click", () => {
+  output.textContent = "Button works. JS is connected.";
+});
